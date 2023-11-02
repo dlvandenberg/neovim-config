6d68599f4b100b3ec4292a2a32e27778ff96d53e
Dennis van den Berg <dennis.vandenberg@nedap.com>
2023-11-02 21:10:01 +0100

    Added linting config

---
+++ lua/dennis/plugins/linting.lua
@@ -0,0 +1,33 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local lint = require("lint")
+
+    lint.linters_by_ft = {
+      javascript = { "eslint_d" },
+      typescript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      svelte = { "eslint_d" },
+    }
+
+    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+    vim.api.nvim_create_autocmd({
+      "BufEnter", -- When opening a buffer or moving into it
+      "BufWritePost", -- When saving a buffer
+      "InsertLeave", -- When leaving insert mode in a buffer
+    }, {
+      group = lint_augroup,
+      callback = function()
+        -- executed when a event is triggered
+        lint.try_lint()
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>l", function()
+      lint.try_lint()
+    end, { desc = "Trigger [L]int" })
+  end,
+}
--- lua/dennis/plugins/lsp/mason.lua
+++ lua/dennis/plugins/lsp/mason.lua
@@ -1,48 +1,57 @@
 return {
-    'williamboman/mason.nvim',
-    dependencies = {
-        'williamboman/mason-lspconfig.nvim',
-        'VonHeikemen/lsp-zero.nvim',
-        'neovim/nvim-lspconfig',
-    },
-    config = function()
-        local mason = require('mason')
-        local mason_lspconfig = require('mason-lspconfig')
-        local lsp_zero = require('lsp-zero')
-        local lspconfig = require('lspconfig')
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "VonHeikemen/lsp-zero.nvim",
+    "neovim/nvim-lspconfig",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local lsp_zero = require("lsp-zero")
+    local lspconfig = require("lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
 
-        mason.setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "→",
-                    package_uninstalled = "✕",
-                }
-            }
-        })
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "→",
+          package_uninstalled = "✕",
+        },
+      },
+    })
 
-        mason_lspconfig.setup({
-            ensure_installed = {
-                "tsserver",
-                "html",
-                "cssls",
-                "svelte",
-                "lua_ls",
-                "graphql",
-                "emmet_ls",
-                "eslint",
-            },
-            -- auto install configured servers
-            automatic_installation = true,
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "tsserver",
+        "html",
+        "cssls",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "eslint",
+      },
+      -- auto install configured servers
+      automatic_installation = true,
 
-            handlers = {
-                lsp_zero.default_setup,
-                lua_ls = function()
-                    local lua_opts = lsp_zero.nvim_lua_ls()
-                    lspconfig.lua_ls.setup(lua_opts)
-                end,
-            }
-        })
-    end
+      handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+          local lua_opts = lsp_zero.nvim_lua_ls()
+          lspconfig.lua_ls.setup(lua_opts)
+        end,
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier",
+        "stylua",
+        "eslint_d",
+      },
+    })
+  end,
 }
-
